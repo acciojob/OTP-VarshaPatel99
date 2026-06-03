ed1adc861d1d22e8414c05ee9eded59149ea4764
VarshaPatel99 <kumarivarsharani2002@gmail.com>
2026-06-03 23:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+if (codes.length > 0) {
+  codes[0].focus();
+}
+
+codes.forEach((code, index) => {
+  code.addEventListener("input", (e) => {
+    e.target.value = e.target.value.slice(0, 1);
+
+    if (e.target.value && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value === "" && index > 0) {
+        codes[index - 1].focus();
+      }
+    }
+  });
+});
